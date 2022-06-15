ffe3e1a3e78313369a31a7165d92f9f8e96f99d9
SideSwipeSF <landscorp@gmail.com>
2022-06-14 22:21:27 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import solid from '@astrojs/solid-js';
+import react from "@astrojs/react";
+import solid from "@astrojs/solid-js";
 
-export default defineConfig({ integrations: [solid()] });
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), solid()]
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,6 @@ module.exports = {
 			sans: [
 				'"Inter", -apple-system,BlinkMacSystemFont, "Segoe UI", "Roboto", sans-serif',
 			],
-			serif: ['freight-text-pro, serif'],
-			heading: [
-				'"Montserrat", -apple-system,BlinkMacSystemFont, "Segoe UI", "Roboto", sans-serif',
-			],
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
 };
